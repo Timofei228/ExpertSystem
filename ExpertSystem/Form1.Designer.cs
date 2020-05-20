b3a3b4b9737ea761e2a922b4663ac3723721fdc9
timur.zhumadilov <timur.zhumadilov@hotmail.com>
2020-05-20 15:29:15 +0600

    Initial commit

---
+++ ExpertSystem/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace ExpertSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Quest = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_Rz = new System.Windows.Forms.ListBox();
+            this.lb_Story = new System.Windows.Forms.ListBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_select = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Quest
+            // 
+            this.lbl_Quest.AutoSize = true;
+            this.lbl_Quest.Location = new System.Drawing.Point(12, 21);
+            this.lbl_Quest.Name = "lbl_Quest";
+            this.lbl_Quest.Size = new System.Drawing.Size(56, 17);
+            this.lbl_Quest.TabIndex = 0;
+            this.lbl_Quest.Text = "Вопрос";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Варианты";
+            // 
+            // lb_Rz
+            // 
+            this.lb_Rz.FormattingEnabled = true;
+            this.lb_Rz.ItemHeight = 16;
+            this.lb_Rz.Location = new System.Drawing.Point(15, 93);
+            this.lb_Rz.Name = "lb_Rz";
+            this.lb_Rz.Size = new System.Drawing.Size(366, 164);
+            this.lb_Rz.TabIndex = 2;
+            this.lb_Rz.SelectedIndexChanged += new System.EventHandler(this.lb_Rz_SelectedIndexChanged);
+            // 
+            // lb_Story
+            // 
+            this.lb_Story.FormattingEnabled = true;
+            this.lb_Story.ItemHeight = 16;
+            this.lb_Story.Location = new System.Drawing.Point(438, 93);
+            this.lb_Story.Name = "lb_Story";
+            this.lb_Story.Size = new System.Drawing.Size(491, 164);
+            this.lb_Story.TabIndex = 3;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(15, 276);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(188, 48);
+            this.btn_Start.TabIndex = 4;
+            this.btn_Start.Text = "Консультация";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(435, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "История ответов";
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(810, 369);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(119, 49);
+            this.btn_close.TabIndex = 6;
+            this.btn_close.Text = "Выход";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(246, 276);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(135, 48);
+            this.btn_select.TabIndex = 7;
+            this.btn_select.Text = "Выбор";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Ответ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 385);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(290, 22);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.lb_Story);
+            this.Controls.Add(this.lb_Rz);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_Quest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Quest;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lb_Rz;
+        private System.Windows.Forms.ListBox lb_Story;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ExpertSystem/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpertSystem
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection myConn;
+        public string connectString = @"Provider=Microsoft.Jet.OLEDB.4.0; Data Source=carsExpertSystem.mdb";
+        public int question_counter = 1;
+        public OleDbCommand cmd;
+        public OleDbDataReader reader;
+        public int NFact;
+        public int NRz;
+        public int Fact_Count = 0;
+        public int NPrav = 0;
+        public int count_mark = 0;
+
+        public Form1()
+        { 
+            InitializeComponent();
+            connection();
+            string count = "SELECT COUNT(IDFact) FROM Facts WHERE type = 0 ";
+            cmd = new OleDbCommand(count, myConn);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Fact_Count = Convert.ToInt32(reader[0]);
+            }
+            myConn.Close();
+        }
+        void consultation()
+        {
+            lb_Rz.Items.Clear();
+            connection();
+            string zap = "SELECT DISTINCT Facts.IDFact, Facts.question, Rz.NameRz " +
+                "FROM Rz INNER JOIN Facts ON Rz.NumFact = Facts.IDFact " +
+                "WHERE Facts.IDFact = @qc";
+            cmd = new OleDbCommand(zap, myConn);
+            cmd.Parameters.AddWithValue("@qc", question_counter);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                lbl_Quest.Text = reader.GetValue(1).ToString();
+                lb_Rz.Items.Add(reader[2].ToString());
+                NFact = Convert.ToInt32(reader[0]);
+            }
+            reader.Close();
+            myConn.Close();
+            count_mark = 0;
+            NPrav = 0;
+        }
+
+        void Clear()
+        {
+            lb_Rz.Items.Clear();
+            lb_Story.Items.Clear();
+            lbl_Quest.Text = "Вопрос";
+        }
+        void connection()
+        {
+            myConn = new OleDbConnection(connectString);
+            myConn.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lb_Rz_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            Clear();
+            consultation();
+            connection();
+            string upd = "UPDATE Posilki SET Posilki.Mark = 0 WHERE Posilki.Mark = 1 OR Posilki.Mark = -1";
+            cmd = new OleDbCommand(upd, myConn);
+            cmd.ExecuteNonQuery();
+            myConn.Close();
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            int Nq = 0;
+            connection();
+            string TxtRz = lb_Rz.SelectedItem.ToString();
+            string story_question = lbl_Quest.Text;
+            string zap = "SELECT IDRz FROM Rz WHERE NameRz = @name ";
+            cmd =  new OleDbCommand(zap, myConn);
+            cmd.Parameters.AddWithValue("@name", TxtRz);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                NRz = Convert.ToInt32(reader[0]);
+            }
+            reader.Close();
+            lb_Story.Items.Add(story_question + " - " + TxtRz);
+            string upd = "UPDATE Posilki SET Posilki.Mark = 1 WHERE Posilki.NumFact = " + NFact + " AND Posilki.NumRz = " + NRz + ";";
+            cmd = new OleDbCommand(upd, myConn);
+            cmd.ExecuteNonQuery();
+            string upd2 = "UPDATE Posilki SET Posilki.Mark = -1 WHERE Posilki.NumFact = " + NFact + " AND Posilki.Mark = 0;";
+            cmd = new OleDbCommand(upd2, myConn);
+            cmd.ExecuteNonQuery();
+            string prav = "SELECT NumRule, SUM(Mark) FROM Posilki GROUP BY NumRule HAVING SUM(Mark) > 0;";
+            cmd = new OleDbCommand(prav, myConn);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                NPrav = Convert.ToInt32(reader[0]);
+            }
+            reader.Close();
+
+            string concl = "SELECT AVG(Mark) FROM Posilki WHERE NumRule = " + NPrav + "";
+            cmd = new OleDbCommand(concl, myConn);
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                try
+                {
+                    count_mark = Convert.ToInt32(reader[0]);
+                }
+                catch (Exception ex )
+                {
+                    MessageBox.Show(ex.Message);
+                    //throw;
+                }
+                finally
+                {
+                    this.Refresh();
+                }
+
+            }
+            textBox1.Text = count_mark.ToString();
+
+            if (count_mark == 1)
+            {
+                string conclusion = "SELECT Facts_1.nameFact, Rz_1.NameRz, Posilki.Mark " +
+                    "FROM((((Posilki INNER JOIN Zakl ON Posilki.NumRule = Zakl.IDZakl) " +
+                    "INNER JOIN Facts AS Facts_1 ON Zakl.NumFact = Facts_1.IDFact) " +
+                    "INNER JOIN Rz AS Rz_1 ON Zakl.NumRz = Rz_1.IDRz) " +
+                    "INNER JOIN Facts ON Posilki.NumFact = Facts.IDFact) " +
+                    "INNER JOIN Rz ON(Posilki.NumRz = Rz.IDRz) " +
+                    "AND(Rz.NumFact = Facts.IDFact) WHERE IDZakl = " + NPrav + "; ";
+                cmd = new OleDbCommand(conclusion, myConn);
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    textBox1.Text = reader[0].ToString() + "-" + reader[1].ToString();
+                }
+
+                MessageBox.Show("Ответ найден");
+                question_counter = 1;
+            }
+            else
+            {
+
+                string qstn = "SELECT NumFact FROM Posilki WHERE Posilki.NumRule = " + NPrav + " AND Mark = 0;";
+                cmd = new OleDbCommand(qstn, myConn);
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Nq = Convert.ToInt32(reader[0]);
+                }
+                reader.Close();
+                question_counter = Nq;
+                consultation();
+            }
+           
+        }
+    }
+}
